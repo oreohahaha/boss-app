b0aca6539d5190cdfb07007c6e676f17c4075579
Oreo <lichen991118@gmail.com>
2018-10-12 17:25:31 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './config';
+
+class App extends Component {
+  render() {
+    return (
+      <div>asd</div>
+    )
+  }
+}
+
+export default App;
---
+++ src/views/login/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Login extends Component{
+  render() {
+    return (
+        <h1>login pages</h1>
+    )
+  }
+}
+
+export default Login;
